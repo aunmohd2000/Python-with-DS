66661467c04465f8d013603b4a30238dcbcec5f9
Syed Aun Mohd <aunmohd280@gmail.com>
2022-07-22 12:11:29 -0700

    update

---
+++ t2.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+forward(2000)#move forward 200 units
+left(90)    #move left 90 degrees
+forward(100)
+left(90)
+forward(2000)
+left(90)
+forward(100)
+
+mainloop()#stops the window from closing
